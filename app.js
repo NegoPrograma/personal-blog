633c548ded90409ba09fbf0ef221daaa32354994
Isaac de Souza <isaacde2012@gmail.com>
2019-07-20 22:06:43 -0300

    Blog já funcional, basta integrar ao BD agora.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const rf = require( __dirname + "/route_formatting.js");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,11 +16,20 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const posts = []
+const posts = [];
 
-app.get("/",(req,res)=>{
-console.log(posts);
+app.get("/posts",(req,res)=>{
   res.render("home",{homeLorem: homeStartingContent, posts: posts});
+});
+
+app.get("/posts/:post",(req,res)=>{
+  let actual_post;
+  posts.forEach( (post) => {
+    if(rf.route_formatting(req.params.post) == rf.route_formatting(post.postTitle));
+          console.log("match found!");
+          actual_post = post;
+  });
+  res.render("unique_post",{ post: actual_post});
 });
 
 
@@ -41,13 +51,11 @@ app.get("/compose",(req,res)=>{
 
 app.post("/compose",(req,res)=>{ 
   let post = {
-  postTitle: req.body.postTitle,
-  postText: req.body.postText
-};
-posts.push(post);
-res.redirect("/");
-  
-
+            postTitle: req.body.postTitle,
+            postText: req.body.postText
+  };
+  posts.push(post);
+  res.redirect("/posts");
 });
   
 
